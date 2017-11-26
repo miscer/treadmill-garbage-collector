e43f398a0f65a9050cf696730e344f38a4c70e52
Michal Miškerník <michal@miskernik.sk>
2017-11-26 20:50:06 +0000

    Tests for scan_step

--- treadmill/heap_test.py
+++ treadmill/heap_test.py
@@ -6,13 +6,110 @@ def dummy_get_roots():
     return ()
 
 
-def dummy_get_children():
+def dummy_get_children(obj):
     return ()
 
 
 def test_create_free_cells():
     first = create_free_cells(10)
     assert len(list(iterate(first))) == 10
+
+
+def test_scan_step_marks_all_children():
+    def get_children(obj):
+        return [cells[0], cells[1]]
+
+    heap = Heap(get_roots=dummy_get_roots,
+                get_children=get_children,
+                initial_size=4)
+
+    cells = list(heap.free)
+
+    heap.free = cells[3]
+    heap.scan = cells[2]
+    heap.top = cells[1]
+    heap.bottom = cells[0]
+
+    cells[2].mark = heap.live_mark
+
+    assert cells[0].mark != heap.live_mark
+    assert cells[1].mark != heap.live_mark
+
+    assert heap.num_scanned == 0
+
+    continue_scan = heap.scan_step()
+
+    assert continue_scan
+
+    assert list(heap.free) == [cells[3], cells[1], cells[0], cells[2]]
+
+    assert heap.scan == cells[0]
+    assert heap.top is None
+    assert heap.bottom == cells[1]
+
+    assert cells[0].mark == heap.live_mark
+    assert cells[1].mark == heap.live_mark
+
+    assert heap.num_scanned == 1
+
+
+def test_scan_step_stops_and_collects():
+    heap = Heap(get_roots=dummy_get_roots,
+                get_children=dummy_get_children,
+                initial_size=10)
+
+    cells = list(heap.free)
+
+    # cells 7-9 are free, cells 4-6 are live, cells 0-3 are garbage
+    heap.free = cells[7]
+    heap.scan = cells[4]
+    heap.top = cells[3]
+    heap.bottom = cells[0]
+
+    heap.num_free = 3
+    heap.num_scanned = 3
+
+    continue_scan = heap.scan_step()
+    assert not continue_scan
+
+    # cells 7-9 and 0-3 are free, cells 4-6 are live
+    assert heap.free == cells[7]
+    assert heap.scan is None
+    assert heap.top is None
+    assert heap.bottom == cells[4]
+
+    assert heap.num_free == 6
+
+
+def test_scan_step_restarts_if_no_garbage():
+    def get_roots():
+        return [cells[6]]
+
+    heap = Heap(get_roots=get_roots,
+                get_children=dummy_get_children,
+                initial_size=10)
+
+    cells = list(heap.free)
+
+    # cells 7-9 are free, cells 0-6 are live, no garbage
+    heap.free = cells[7]
+    heap.scan = cells[0]
+    heap.top = None
+    heap.bottom = cells[0]
+
+    heap.num_free = 3
+    heap.num_scanned = 7
+
+    continue_scan = heap.scan_step()
+    assert not continue_scan
+
+    # cells 7-9 are free, cells 0-6 are live, scan is restarted
+    assert heap.free == cells[7]
+    assert heap.scan == cells[6]
+    assert heap.top == cells[5]
+    assert heap.bottom == cells[0]
+
+    assert heap.num_scanned == 0
 
 
 def test_collect():
