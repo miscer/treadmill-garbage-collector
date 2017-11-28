c17101f3d5eb9094626c50d0568d9b093a218716
Michal Miškerník <michal@miskernik.sk>
2017-11-28 22:59:08 +0000

    Analysis

--- linked_list.py
+++ linked_list.py
@@ -3,6 +3,10 @@ from episcopal.runtime import Indirection
 from treadmill import Heap
 
 
+heap = None
+episcopal_list = None
+
+
 def heap_roots():
     return [episcopal_list] if episcopal_list else []
 
@@ -29,20 +33,21 @@ def list_add(head):
     return new_head
 
 
-def create_list():
+def create_list(size):
     global episcopal_list
 
     episcopal_list = list_init()
 
-    for _ in range(100):
+    for _ in range(size - 1):
         episcopal_list = list_add(episcopal_list)
 
 
-heap = Heap(heap_roots, heap_children, initial_size=10, scan_step_size=5)
-print(heap.string())
-
-for _ in range(10):
-    episcopal_list = None
-    create_list()
-
+if __name__ == '__main__':
+    heap = Heap(heap_roots, heap_children, initial_size=10, scan_step_size=5)
     print(heap.string())
+
+    for _ in range(10):
+        episcopal_list = None
+        create_list(100)
+
+        print(heap.string())
